bfe00a8c62c67d9a1f8c5dd6c1e563ec3a88e842
wilson048 <therealsponge7@gmail.com>
2023-09-06 19:24:14 -0500

    Made Initial Grid

--- Homework-1/Terrain-Generation.c
+++ Homework-1/Terrain-Generation.c
@@ -3,5 +3,32 @@
 #include <math.h>
 
 int main(int argc, char *argv[]) {
-    return 0;
+    // Generate board
+    char *terrain[21][80];
+    int x, y;
+    // Generate top-bottom border
+    for(x = 0; x < 80; x++) {
+        terrain[0][x] = "\%";
+        terrain[20][x] = "\%";
+    }
+
+    for(x = 1; x < 20; x++) {
+        for(y = 0; y < 80; y++) {
+            if(y == 0 || y == 79) {
+                terrain[x][y] = "\%";
+            }
+            else {
+                terrain[x][y] = ".";
+            }
+        }
+    }
+
+    // Print Grid
+    for(x = 0; x < 21; x++) {
+        for(y = 0; y < 80; y++) {
+            printf("%s ", terrain[x][y]);
+        }
+        printf("\n");
+    }
+    
 }
