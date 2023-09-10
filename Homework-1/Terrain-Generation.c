0741648719d502f590869f575c10875615e99774
wilson048 <therealsponge7@gmail.com>
2023-09-09 22:16:43 -0500

    Made mart generation

--- Homework-1/Terrain-Generation.c
+++ Homework-1/Terrain-Generation.c
@@ -30,67 +30,176 @@ void initalize_grid(char *terrain[21][80]) {
     }
 }
 void generate_path_and_shops(char *terrain[21][80]) {
-    int start_index, end_index, shift_index;
+    int h_start_index, h_end_index, h_shift_index;
     // Generate West-East path
-    start_index = (rand()) % (18 - 2 + 1) + 2;
-    end_index = (rand()) % (18 - 2 + 1) + 2;
+    h_start_index = (rand()) % (18 - 2 + 1) + 2;
+    h_end_index = (rand()) % (18 - 2 + 1) + 2;
     // Prevent straight paths
-    while(start_index == end_index) {
-        start_index = (rand()) % (18 - 2 + 1) + 2;
-        end_index = (rand()) % (18 - 2 + 1) + 2;
+    while(h_start_index == h_end_index) {
+        h_start_index = (rand()) % (18 - 2 + 1) + 2;
+        h_end_index = (rand()) % (18 - 2 + 1) + 2;
     }
-    shift_index = (rand()) % (60 - 30 + 1) + 30;
+    h_shift_index = (rand()) % (60 - 30 + 1) + 30;
     int x, y;
     // Generate start of path
-    for(x = 0; x < shift_index; x++) {
-        terrain[start_index][x] = "#";
+    for(x = 0; x < h_shift_index; x++) {
+        terrain[h_start_index][x] = "#";
     }
     // Generate North-South offset upward
-    if(start_index > end_index) {
-        for(y = start_index; y > end_index; y--) {
-            terrain[y][shift_index] = "#";
+    if(h_start_index > h_end_index) {
+        for(y = h_start_index; y > h_end_index; y--) {
+            terrain[y][h_shift_index] = "#";
         }
     }
     // Generate North-South offset downward
     else {
-        for(y = start_index; y < end_index; y++) {
-            terrain[y][shift_index] = "#";
+        for(y = h_start_index; y < h_end_index; y++) {
+            terrain[y][h_shift_index] = "#";
         }
     }
     // Generate end of path
-    for(x = shift_index; x < 80; x++) {
-        terrain[end_index][x] = "#";
+    for(x = h_shift_index; x < 80; x++) {
+        terrain[h_end_index][x] = "#";
     }
+    int v_start_index, v_end_index, v_shift_index;
     // Generate North-South path
-    start_index = (rand()) % (78 - 2 + 1) + 2;
-    end_index = (rand()) % (78 - 2 + 1) + 2;
+    v_start_index = (rand()) % (78 - 2 + 1) + 2;
+    v_end_index = (rand()) % (78 - 2 + 1) + 2;
     // Prevent straight paths
-    while(start_index == end_index) {
-        start_index = (rand()) % (78 - 1 + 1) + 1;
-        end_index = (rand()) % (78 - 1 + 1) + 1;
+    while(v_start_index == v_end_index) {
+        v_start_index = (rand()) % (78 - 1 + 1) + 1;
+        v_end_index = (rand()) % (78 - 1 + 1) + 1;
     }
-    shift_index = end_index = (rand()) % (15 - 5 + 1) + 5;
+    v_shift_index = v_end_index = (rand()) % (15 - 5 + 1) + 5;
     // Generate start of path
-    for(y = 0; y < shift_index; y++) {
-        terrain[y][start_index] = "#";
+    for(y = 0; y < v_shift_index; y++) {
+        terrain[y][v_start_index] = "#";
     }
     // Generate North-South offset right
-    if(start_index > end_index) {
-        for(x = start_index; x > end_index; x--) {
-            terrain[shift_index][x] = "#";
+    if(v_start_index > v_end_index) {
+        for(x = v_start_index; x > v_end_index; x--) {
+            terrain[v_shift_index][x] = "#";
         }
     }
     // Generate North-South offset left
     else {
-        for(x = start_index; x < end_index; x++) {
-            terrain[shift_index][x] = "#";
+        for(x = v_start_index; x < v_end_index; x++) {
+            terrain[v_shift_index][x] = "#";
         }
     }
     // Generate end of path
-    for(y = shift_index; y < 21; y++) {
-        terrain[y][end_index] = "#";
+    for(y = v_shift_index; y < 21; y++) {
+        terrain[y][v_end_index] = "#";
     }
-
+    // Left/North on 1, Right/South on 2
+    int mart_orientation = (rand()) % (2 - 1 + 1) + 1;
+    int pkm_orientation = (rand()) % (2 - 1 + 1) + 1;
+    // Horizontal on 1, Vertical on 2
+    int path_orientation = (rand()) % (2 - 1 + 1) + 1;
+    int m_location, pkm_location;
+    // Generate on Horizontal path
+    if(path_orientation == 1) {
+        m_location = (rand()) % (78 - 2 + 1) + 2;
+        while(m_location == h_shift_index) {
+            m_location = (rand()) % (78 - 2 + 1) + 2;
+        }
+        if(mart_orientation == 1) {
+            // Generate mart at start index side, north of the horizontal path
+            if(m_location > h_shift_index)  {
+                terrain[h_end_index - 1][m_location] = "M";
+            }
+            // Generate mart at end index side, north of the horizontal path
+            else {
+                terrain[h_start_index - 1][m_location] = "M";
+            }
+        }
+        else {
+            // Generate mart at start index side, south of the horizontal path
+            if(m_location > h_shift_index)  {
+                terrain[h_end_index + 1][m_location] = "M";
+            }
+            // Generate mart at end index side, south of the horizontal path
+            else {
+                terrain[h_start_index + 1][m_location] = "M";
+            }
+        }
+        // Set pokemon center location
+        pkm_location = (rand()) % (78 - 2 + 1) + 2;
+        while(pkm_location == h_shift_index || pkm_location == m_location) {
+            pkm_location = (rand()) % (78 - 2 + 1) + 2;
+        }
+        if(pkm_orientation == 1) {
+            // Generate PokeCenter at start index side, north of the horizontal path
+            if(pkm_location > h_shift_index)  {
+                terrain[h_end_index - 1][pkm_location] = "P";
+            }
+            // Generate PokeCenter at end index side, north of the horizontal path
+            else {
+                terrain[h_start_index - 1][pkm_location] = "P";
+            }
+        }
+        else {
+            // Generate PokeCenter at start index side, south of the horizontal path
+            if(m_location > h_shift_index)  {
+                terrain[h_end_index + 1][pkm_location] = "P";
+            }
+            // Generate PokeCenter at end index side, south of the horizontal path
+            else {
+                terrain[h_start_index + 1][pkm_location] = "P";
+            }
+        }   
+    }
+    else {
+        m_location = (rand()) % (18 - 2 + 1) + 2;
+        while(m_location == v_shift_index) {
+            m_location = (rand()) % (18 - 2 + 1) + 2;
+        }
+        if(mart_orientation == 1) {
+            // Generate mart at start index side, left of the vertical path
+            if(m_location < v_shift_index)  {
+                terrain[m_location][v_start_index - 1] = "M";
+            }
+            // Generate mart at end index side, left of the vertical path
+            else {
+                terrain[m_location][v_end_index - 1] = "M";
+            }
+        }
+        else {
+            // Generate mart at start index side, right of the vertical path
+            if(m_location < v_shift_index)  {
+                terrain[m_location][v_start_index + 1] = "M";
+            }
+            // Generate mart at end index side, right of the vertical path
+            else {
+                terrain[m_location][v_end_index + 1] = "M";
+            }
+        }
+        // Set pokemon center location
+        pkm_location = (rand()) % (18 - 2 + 1) + 2;
+        while(pkm_location == v_shift_index || pkm_location == m_location) {
+            pkm_location = (rand()) % (18 - 2 + 1) + 2;
+        }
+        if(pkm_orientation == 1) {
+            // Generate PokeCenter at start index side, left of the vertical path
+            if(pkm_location < v_shift_index)  {
+                terrain[pkm_location][v_start_index - 1] = "P";
+            }
+            // Generate PokeCenter at end index side, left of the vertical path
+            else {
+                terrain[pkm_location][v_end_index - 1] = "P";
+            }
+        }
+        else {
+            // Generate PokeCenter at start index side, right of the vertical path
+            if(m_location > h_shift_index)  {
+                terrain[pkm_location][v_start_index + 1] = "P";
+            }
+            // Generate PokeCenter at end index side, south of the vertical path
+            else {
+                terrain[pkm_location][v_end_index + 1] = "P";
+            }
+        }
+    }
 
 }
 
