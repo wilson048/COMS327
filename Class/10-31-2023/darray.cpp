1c621e5a4aab5c4c4ddceebef1a965d4a4309752
wilson048 <therealsponge7@gmail.com>
2023-11-02 11:08:01 -0500

    Add 11-2 class

---
+++ Class/10-31-2023/darray.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "darray.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    darray<int> ai;
+    darray<string> as;
+    int i;
+    for(i = 0; i < 100; i++) {
+        ai.add(i);
+    }
+    cout << ai << endl;
+    ai.remove(42);
+
+    ai.set(10, 25);
+    ai[5] = 50;
+    ai.get(75) = 25;
+
+    cout << ai << endl;
+    as.add("Hello0");
+    cout << as << endl;
+    try {
+        for(i = 0;; i++) {
+            ai[i] = i;
+            cout << ai[i] << endl;
+        }
+    }
+    catch(const char *s) {
+        cout << s << endl;
+    }
+    return 0;
+}
--- Class/10-31-2023/darray.h
+++ Class/10-31-2023/darray.h
@@ -1,25 +1,97 @@
 #ifndef DARRAY_H
 #define DARRAY_H
-template <class T>
 
-#define DEFAULT_SIZE = 10
+#include <iostream>
+#include <exception>
+
+#define DEFAULT_SIZE 10
+
+template <class T>
 
 class darray {
     private:
         T *a;
         int size;
+        int caboose;
+        void resize() {
+            T *tmp;
+            int i;
+            tmp = new T[size * 2];
+            for (i = 0; i < size; i++) {
+                tmp[i] = a[i];
+            }
 
-        void resize();
+            delete [] a;
+            a = tmp;
+            size *= 2;
+        };
     public:
-        darray() : a(new T[DEFAULT_SIZE]), size(DEFAULT_SIZE) {};
-        darray(int size);
-        ~darray();
+        class iterator {
+            private:
+                int itr;
+            public:
+                iterator() : itr(0) {}
+                void operator++() {
+                    if((itr + 1) < caboose) {
+                        itr++;
+                    }
+                }
+                // iterator start() {
+                //     iterator i;
+                //     return i;
+                // }
+                T &operator*() {
+                    return a[itr];
+                }
+        };
 
-        T  &operator[](int i);
-        void add(const T &t);
-        ostream &print(ostream &o);
-        void set(int i, const T &t);
-        T remove(int i);
-        T &get(int i);
+        darray() : a(new T[DEFAULT_SIZE]), size(DEFAULT_SIZE), caboose(0) {}
+        darray(int s) : a(new T[s]), size(s), caboose(0) {}
+        ~darray() {
+            delete [] a;
+        }
+
+        T  &operator[](int i) {
+            if(i >= size) {
+                throw "Index out of bounds";
+            }
+            return a[i];
+        }
+        void add(const T &t) {
+            if(caboose == size) {
+                resize();
+            }
+            a[caboose++] = t;
+        }
+        void set(int i, const T &t) {
+            a[i] = t;
+        }
+        T remove(int i) {
+            int j;
+            T tmp;
+            tmp = a[i];
+            for (j = i + 1; j < caboose; j++) {
+                a[j - 1] = a[j];
+            }
+            caboose--;
+            return tmp;
+        }
+        T &get(int i) {
+            return a[i];
+        }
+        std::ostream &print(std::ostream &o) const {
+            int i;
+            o << "darray[" << std::endl;
+            for(i = 0; i < caboose; i++) {
+                o << " " << a[i] <<  "," << std::endl;
+            }
+            o << "]" << std::endl;
+            return o;
+        }
+};
+
+template <class T>
+std::ostream &operator<<(std::ostream &o, const darray<T> &a) {
+    return a.print(o);
 }
 #endif
