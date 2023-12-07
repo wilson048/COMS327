7ae04ab9ecd0fa381bd0e5d683831b12f7140334
wilson048 <therealsponge7@gmail.com>
2023-12-07 03:05:12 -0600

    Start making strucutre for Connect 4

--- Homework-10/connect_four.cpp
+++ Homework-10/connect_four.cpp
@@ -1,6 +1,27 @@
+#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
 #include <cstring>
 #include <ncurses.h>
 
+typedef enum {
+    PLAYER_ONE_TOKEN,
+    PLAYER_TWO_TOKEN,
+    NO_ONE_TOKEN 
+} type_token;
+
+int grid[6][7];
+
 int main(int arg, char *argv[]) {
+    initscr();
+    raw();
+    noecho();
+    int i, j;
+    for(i = 0; i < 6; i++) {
+        for(j = 0; j < 7; j++) {
+            grid[i][j] = NO_ONE_TOKEN;
+        }
+    }
+
     return 0;
 }   
